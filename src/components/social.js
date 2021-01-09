baddf15f763741355ed0852bedd06aea4c419732
joshhy99 <koltongkoltong3@gmail.com>
2021-01-09 00:02:37 -0800

    Original

--- src/components/social.js
+++ src/components/social.js
@@ -12,7 +12,7 @@ class Social extends Component {
             <div className="socialLinks">
                 <div><a className="hvr-icon-shrink" href="https://www.linkedin.com/in/joshua-yun-9709b0170/" rel="noopener noreferrer" target="_blank"><img className="hvr-icon" style={{width:"26px" }} src={linkedin} alt="https://www.linkedin.com/in/joshua-yun-9709b0170/"></img></a></div>
                 <div><a className="hvr-icon-shrink" href="https://www.facebook.com/joshposh555/" rel="noopener noreferrer" target="_blank"><img className="hvr-icon" style={{maxWidth:"25px"}} src={fb} alt="https://www.facebook.com/joshposh555/"></img></a></div>
-                <div><a className="hvr-icon-shrink" href="https://github.com/fizzyjosh555" rel="noopener noreferrer" target="_blank"><img className="hvr-icon" src={gh} style={{maxWidth:"25px"}} alt="https://github.com/fizzyjosh555"></img></a></div>
+                <div><a className="hvr-icon-shrink" href="https://github.com/fizzyjosh555" rel="noopener noreferrer" target="_blank"><img className="hvr-icon" src={gh} style={{ paddingLeft: '1px', maxWidth:"25px"}} alt="https://github.com/fizzyjosh555"></img></a></div>
                 <div><a className="hvr-icon-shrink" href={resFile} rel="noopener noreferrer" target="_blank"><img className="hvr-icon" src={res} style={{maxWidth:"25px"}} alt="Resume"></img></a></div>
                 <div><a className="hvr-icon-shrink" href="mailto:kwbin6022@gmail.com" rel="noopener noreferrer" target="_blank"><img className="hvr-icon" src={mes} style={{maxWidth:"25px"}} alt="Message"></img></a></div>
             </div>
