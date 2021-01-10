26cb1a2b1e33bc044826b500171881aa64f0e4d6
joshhy99 <koltongkoltong3@gmail.com>
2021-01-09 22:34:15 -0800

    Final

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './css/App.css';
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 
 import Navbar from './components/Navbar.js';
 import Intro from './contents/Intro';
